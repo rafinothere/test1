8c7fa425e05c70699fcf745c0761f729b4a5c61d
rafinothere <rafiul00751@gmail.com>
2025-08-22 01:14:45 +0100

    Reapply "a"
    This reverts commit 6984def84ba4c8d4adb6467f13342711434b67b1.

--- mindspring-tuition/vite.config.js
+++ mindspring-tuition/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 👇 VERY IMPORTANT — change base depending on deployment
+  base: '/MINDSPRING-TUITION/', 
+});
